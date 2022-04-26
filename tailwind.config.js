f65d1cbe73b50ecac5716c155b3d6dc55bc9a10c
Afochar Syabnirusmanto <aflowcharsss@gmail.com>
2022-04-27 06:43:34 +0800

    Header - Content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx,css}"],
   theme: {
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
